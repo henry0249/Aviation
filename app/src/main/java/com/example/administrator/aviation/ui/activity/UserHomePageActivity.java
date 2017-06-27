67a26c6ae6a41075347a801c921f2475260b4449
595731157 <595731157@qq.com>
2017-06-27 16:16:49 +0800

    更新在首页

--- app/src/main/java/com/example/administrator/aviation/ui/activity/UserHomePageActivity.java
+++ app/src/main/java/com/example/administrator/aviation/ui/activity/UserHomePageActivity.java
@@ -1,10 +1,18 @@
 package com.example.administrator.aviation.ui.activity;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.Fragment;
@@ -23,6 +31,17 @@ import com.example.administrator.aviation.R;
 import com.example.administrator.aviation.ui.base.NavBar;
 import com.example.administrator.aviation.ui.fragment.HomePageFragment;
 import com.example.administrator.aviation.ui.fragment.PersonFragment;
+import com.example.administrator.aviation.util.PreferenceUtils;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 /**
  * 所有用户首页
@@ -56,12 +75,44 @@ public class UserHomePageActivity extends FragmentActivity implements View.OnCli
         }
     };
 
+    // 版本更新（版本号 提示消息 下载地址）
+    private ProgressDialog pBar;
+    private String version;
+    private String describe;
+    private String url;
+    @SuppressLint("HandlerLeak")
+    private Handler handler1 = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (isNeedUpdate()) {
+                showUpdateDialog();
+            } else {
+                Toast.makeText(UserHomePageActivity.this, "当前是最新版本", Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        // 获取版本更新信息
+        version = PreferenceUtils.getAPPVersion(UserHomePageActivity.this);
+        describe = PreferenceUtils.getAPPDescribe(UserHomePageActivity.this);
+        url = PreferenceUtils.getAppUrl(UserHomePageActivity.this);
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    handler1.sendEmptyMessage(0);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
         initView();
-
         initFragment(0);
     }
 
@@ -209,4 +260,135 @@ public class UserHomePageActivity extends FragmentActivity implements View.OnCli
         personIv.setImageResource(R.drawable.wode2);
         personTv.setTextColor(0xff666666);
     }
+
+
+
+    // 判断是否是最新版本，如果不是，跳出对话框选择是否更新
+    private void showUpdateDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(UserHomePageActivity.this);
+        builder.setIcon(android.R.drawable.ic_dialog_info);
+        builder.setTitle("请升级APP至版本" + version);
+        builder.setMessage(describe);
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                    downFile(url);
+
+                    // 调用ApkDownService进行下载
+//                    Intent intent = new Intent(UpdateActivity.this, ApkDownService.class);
+//                    intent.putExtra("apkUrl", info.getUrl());
+//                    startService(intent);
+                } else {
+                    Toast.makeText(UserHomePageActivity.this, "SD卡不可用", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.create().show();
+    }
+
+    private boolean isNeedUpdate() {
+        // 比较新版本和旧版本值是否相等
+        if (version.equals(getVersion())) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // 获取当前版本号
+    private String getVersion() {
+        try {
+            PackageManager packageManager = this.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(this.getPackageName(), 0);
+            String oldVersion = packageInfo.versionName;
+            return oldVersion;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "版本号未知";
+        }
+    }
+
+
+    void downFile(final String url) {
+        // 进度条，在下载的时候实时更新进度，提高用户友好度
+        pBar = new ProgressDialog(UserHomePageActivity.this);
+        pBar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        pBar.setTitle("正在下载");
+        pBar.setMessage("请稍后......");
+        pBar.setProgress(0);
+        pBar.show();
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                HttpClient client = new DefaultHttpClient();
+                HttpGet get = new HttpGet(url);
+                HttpResponse response;
+                try {
+                    response = client.execute(get);
+                    HttpEntity entity = response.getEntity();
+                    // 获取文件大小
+                    int length = (int) entity.getContentLength();
+
+                    // 设置进度条的总长度
+                    pBar.setMax(length);
+
+                    InputStream is = entity.getContent();
+                    FileOutputStream fileOutputStream = null;
+                    if (is != null) {
+                        File file = new File(Environment.getExternalStorageDirectory(), "app-release.apk");
+                        fileOutputStream = new FileOutputStream(file);
+
+                        //这个是缓冲区，即一次读取10个比特，我弄的小了点，
+                        // 因为在本地，所以数值太大一 下就下载完了，看不出progressbar的效果。
+                        byte[] buf = new byte[1024];
+                        int ch = -1;
+                        int process = 0;
+                        while ((ch = is.read(buf)) != -1) {
+                            fileOutputStream.write(buf, 0, ch);
+                            process += ch;
+                            // 这里就是关键的实时更新进度
+                            pBar.setProgress(process);
+                        }
+                    }
+                    assert fileOutputStream != null;
+                    fileOutputStream.flush();
+                    fileOutputStream.close();
+                    down();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+    }
+
+    void down() {
+        handler1.post(new Runnable() {
+            @Override
+            public void run() {
+                pBar.cancel();
+                update();
+            }
+        });
+    }
+
+    // 安装文件，一般固定写法
+    void update() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.fromFile(new File(Environment.getExternalStorageDirectory(), "app-release.apk")),
+                "application/vnd.android.package-archive");
+        startActivity(intent);
+    }
+
 }
